3340cb569e92761e3ba40451d5e0fda1151927c0
Gary Taylor <gary.taylor@hismessages.com>
2014-08-13 15:19:31 +0100

    Moved from unzip to decompress-zip npm module for decompression due to errors occuring on some machines.

--- install.js
+++ install.js
@@ -4,20 +4,34 @@ var path = require('path');
 var fs = require('fs');
 var unzip = require('unzip');
 var destFolder = path.resolve('./src');
+var DecompressZip = require('decompress-zip');
 console.log('Downloading from http://cdn.sencha.io/ext-' + VERSION + '-gpl.zip');
 var download = new Download()
     .get('http://cdn.sencha.io/ext-' + VERSION + '-gpl.zip', path.resolve('./'), { extract: false});
 
 download.run(function (err) {
+    var unzipper;
     if (err) {
         throw err;
     }
     fs.renameSync(path.join('./', 'ext-' + VERSION + '-gpl.zip'), path.join('./', 'ext.zip'));
-    reader = fs.createReadStream(path.join('./', 'ext.zip'));
-    reader.on('close', function () {
+    if(fs.existsSync(path.join(destFolder, 'ext'))){
+        fs.rmdirSync(path.join(destFolder, 'ext'));
+    }
+    unzipper = new DecompressZip(path.resolve('./ext.zip'))
+    unzipper.on('error', function (err) {
+        console.log('Caught an error ' + err.message);
+    });
+    unzipper.on('extract', function (log) {
         fs.renameSync(path.join(destFolder, 'extjs-' + VERSION), path.join(destFolder, 'ext'));
         console.log('Download complete');
-
     });
-    reader.pipe(unzip.Extract({ path: destFolder }));
+    unzipper.extract({
+        path: destFolder,
+        filter: function (file) {
+            return file.type !== "SymbolicLink";
+        }
+    });
+
+
 });
